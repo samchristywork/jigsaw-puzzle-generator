cb88c89de2d2bd8aac5f96eb65bd05ddc94870a1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-01 17:44:23 -0600

    Feature: First rendering of proper puzzle piece
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,15 @@ fn main() {
 
     svg::svg_start(width, height);
 
-    let t = transform::Transform {
+    let mut t = transform::Transform {
         operations: vec![
             Operation {
-                kind: transform::Kind::Offset,
-                v: Vector { x: -0.5, y: -0.5 },
-            },
-            Operation {
-                kind: transform::Kind::Rotate,
-                v: Vector {
-                    x: 3.141 / 2.0,
-                    y: 0.0,
-                },
-            },
-            Operation {
-                kind: transform::Kind::Offset,
+                kind: transform::Kind::Scale,
                 v: Vector { x: 0.5, y: 0.5 },
             },
             Operation {
-                kind: transform::Kind::Scale,
-                v: Vector { x: 0.5, y: 1.0 },
+                kind: transform::Kind::Offset,
+                v: Vector { x: 0.25, y: 0.0 },
             },
         ],
     };
@@ -40,47 +29,68 @@ fn main() {
     svg::path_begin();
     svg::move_to(&t, Vector { x: 0.00, y: 0.50 });
 
-    svg::draw_quadratic_curve(
-        &t,
-        Vector { x: 0.25, y: 0.50 },
-        Vector { x: 0.50, y: 0.50 },
-        Vector { x: 0.25, y: 0.25 },
-    );
-    svg::draw_quadratic_curve(
-        &t,
-        Vector { x: 0.00, y: 0.00 },
-        Vector { x: 1.00, y: 0.00 },
-        Vector { x: 0.75, y: 0.25 },
-    );
-    svg::draw_quadratic_curve(
-        &t,
-        Vector { x: 0.50, y: 0.50 },
-        Vector { x: 0.75, y: 0.50 },
-        Vector { x: 1.00, y: 0.50 },
-    );
+    let oldops = t.operations.clone();
+    for i in 0..4 {
+        t.operations = oldops.clone();
 
-    svg::line_to(&t, Vector { x: 1.0, y: 1.0 });
-    svg::line_to(&t, Vector { x: 0.0, y: 1.0 });
+        t.operations.push(Operation {
+            kind: transform::Kind::Offset,
+            v: Vector { x: -0.5, y: -0.5 },
+        });
+
+        t.operations.push(Operation {
+            kind: transform::Kind::Rotate,
+            v: Vector {
+                x: 3.141 / 2.0 * i as f32,
+                y: 0.0,
+            },
+        });
+
+        t.operations.push(Operation {
+            kind: transform::Kind::Offset,
+            v: Vector { x: 0.5, y: 0.5 },
+        });
+
+        svg::draw_quadratic_curve(
+            &t,
+            Vector { x: 0.25, y: 0.50 },
+            Vector { x: 0.50, y: 0.50 },
+            Vector { x: 0.25, y: 0.25 },
+        );
+        svg::draw_quadratic_curve(
+            &t,
+            Vector { x: 0.00, y: 0.00 },
+            Vector { x: 1.00, y: 0.00 },
+            Vector { x: 0.75, y: 0.25 },
+        );
+        svg::draw_quadratic_curve(
+            &t,
+            Vector { x: 0.50, y: 0.50 },
+            Vector { x: 0.75, y: 0.50 },
+            Vector { x: 1.00, y: 0.50 },
+        );
+    }
+
     svg::path_end("blue", "black", stroke);
 
-    svg::draw_control_points(
-        &t,
-        Vector { x: 0.25, y: 0.50 },
-        Vector { x: 0.50, y: 0.50 },
-        Vector { x: 0.25, y: 0.25 },
-    );
-    svg::draw_control_points(
-        &t,
-        Vector { x: 0.00, y: 0.00 },
-        Vector { x: 1.00, y: 0.00 },
-        Vector { x: 0.75, y: 0.25 },
-    );
-    svg::draw_control_points(
-        &t,
-        Vector { x: 0.50, y: 0.50 },
-        Vector { x: 0.75, y: 0.50 },
-        Vector { x: 1.00, y: 0.50 },
-    );
+    //svg::draw_control_points(
+    //    &t,
+    //    Vector { x: 0.25, y: 0.50 },
+    //    Vector { x: 0.50, y: 0.50 },
+    //    Vector { x: 0.25, y: 0.25 },
+    //);
+    //svg::draw_control_points(
+    //    &t,
+    //    Vector { x: 0.00, y: 0.00 },
+    //    Vector { x: 1.00, y: 0.00 },
+    //    Vector { x: 0.75, y: 0.25 },
+    //);
+    //svg::draw_control_points(
+    //    &t,
+    //    Vector { x: 0.50, y: 0.50 },
+    //    Vector { x: 0.75, y: 0.50 },
+    //    Vector { x: 1.00, y: 0.50 },
+    //);
 
     svg::draw_box(0.0, 0.0, 1.0, 1.0, stroke);
 
