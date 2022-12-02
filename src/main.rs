930b0dd91361971c436fe51daf893d4493e4a4ca
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-01 18:32:46 -0600

    Feature: Added inverted sides
    Added the ability to have a side that goes into the puzzle piece instead
    of just having outside tabs.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,8 @@ pub mod vector;
 use transform::Operation;
 use vector::Vector;
 
-
-fn main() {
-    let width = 1.01;
-    let height = 1.01;
+fn make_piece(mut t: transform::Transform) {
     let stroke = 0.01;
-
-    svg::svg_start(width, height);
-
-    let mut t = transform::Transform {
-        operations: vec![
-            Operation {
-                kind: transform::Kind::Scale,
-                v: Vector { x: 0.5, y: 0.5 },
-            },
-            Operation {
-                kind: transform::Kind::Offset,
-                v: Vector { x: 0.25, y: 0.0 },
-            },
-        ],
-    };
 
     svg::path_begin();
     svg::move_to(&t, Vector { x: 0.00, y: 0.50 });
@@ -35,7 +17,7 @@ fn main() {
 
         t.operations.push(Operation {
             kind: transform::Kind::Offset,
-            v: Vector { x: -0.5, y: -0.5 },
+            v: Vector { x: -0.5, y: -0.75 },
         });
 
         t.operations.push(Operation {
@@ -47,27 +29,48 @@ fn main() {
         });
 
         t.operations.push(Operation {
+            kind: transform::Kind::Scale,
+            v: Vector { x: 0.5, y: 0.5 },
+        });
+
+        t.operations.push(Operation {
             kind: transform::Kind::Offset,
             v: Vector { x: 0.5, y: 0.5 },
         });
 
+        let mut inverted = 0.0;
+        if i == 2 {
+            inverted = 1.0;
+        }
         svg::draw_quadratic_curve(
             &t,
             Vector { x: 0.25, y: 0.50 },
             Vector { x: 0.50, y: 0.50 },
-            Vector { x: 0.25, y: 0.25 },
+            Vector {
+                x: 0.25,
+                y: 0.25 + 0.50 * inverted,
+            },
         );
         svg::draw_quadratic_curve(
             &t,
-            Vector { x: 0.00, y: 0.00 },
-            Vector { x: 1.00, y: 0.00 },
-            Vector { x: 0.75, y: 0.25 },
+            Vector {
+                x: 0.00,
+                y: 0.00 + inverted,
+            },
+            Vector {
+                x: 1.00,
+                y: 0.00 + inverted,
+            },
+            Vector {
+                x: 0.75,
+                y: 0.25 + 0.50 * inverted,
+            }, //75
         );
         svg::draw_quadratic_curve(
             &t,
             Vector { x: 0.50, y: 0.50 },
             Vector { x: 0.75, y: 0.50 },
-            Vector { x: 1.00, y: 0.50 },
+            Vector { x: 1.50, y: 0.50 },
         );
     }
 
@@ -91,6 +94,40 @@ fn main() {
     //    Vector { x: 0.75, y: 0.50 },
     //    Vector { x: 1.00, y: 0.50 },
     //);
+}
+
+fn main() {
+    let width = 1.01;
+    let height = 1.01;
+    let stroke = 0.01;
+
+    svg::svg_start(width, height);
+
+    let mut t = transform::Transform {
+        operations: vec![
+            Operation {
+                kind: transform::Kind::Scale,
+                v: Vector { x: 0.5, y: 0.5 },
+            },
+            Operation {
+                kind: transform::Kind::Offset,
+                v: Vector { x: 0.25, y: 0.0 },
+            },
+        ],
+    };
+
+    make_piece(transform::Transform {
+        operations: t.operations.clone(),
+    });
+
+    //t.operations.push(Operation {
+    //    kind: transform::Kind::Offset,
+    //    v: Vector { x: 0.05, y: 0.00 },
+    //});
+
+    //make_piece(transform::Transform {
+    //    operations: t.operations.clone(),
+    //});
 
     svg::draw_box(0.0, 0.0, 1.0, 1.0, stroke);
 
